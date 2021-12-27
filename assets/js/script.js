3fd96dac94c3b174f4e3434810a2f4662ca5b2ec
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2021-12-27 00:42:17 -0600

    added handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
-buttonEl.addEventListener("click", createTaskHandler); 
+};
+formEl.addEventListener("submit", createTaskHandler); 
+
+// CONTINUE ONTO 4.2.6
